4a6fe1dbc1e5479a4da1789cb2c76a4c2fef8fb8
SvenHaedrich <sven.haedrich@sevenlab.de>
2022-06-22 22:34:20 +0200

    start freeRTOS

--- source/main.c
+++ source/main.c
@@ -1,15 +1,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 #include "board/board.h"
+#include "version.h"
 #include "log/log.h"
 
 int main(void)
 {
+    printf("DALI USB interface - SevenLab 2022\r\n");
+    printf("Version %d.%d.%d \r\n", MAJOR_VERSION_SOFTWARE, MINOR_VERSION_SOFTWARE, BUGFIX_VERSION_SOFTWARE);
+    printf(LOG_BUILD_VERSION);
+
     log_init();
     board_init();
 
-    while (1) {
-    }
-    return 0;
+    vTaskStartScheduler();
 }
