53f20ffbde6ea6070bbdc8a05daf5fb7ecf56da9
SvenHaedrich <sven.haedrich@sevenlab.de>
2022-06-22 22:35:47 +0200

    re-enable some of the timers

--- source/board/board.c
+++ source/board/board.c
@@ -1,9 +1,10 @@
 #include <stdbool.h>
 
-#include "lpc11xx.h"
-#include "bitfields.h"
 #include "FreeRTOS.h"
 #include "timers.h"
+
+#include "lpc11xx.h"
+#include "bitfields.h"
 #include "../main.h"
 #include "../log/log.h"
 #include "board.h"
@@ -12,22 +13,25 @@
 #define SLOW_HEARTBEAT_COUNTER 10
 #define FLASH_PERIOD_MS 200
 
-// static TimerHandle_t board_heartbeat_timer_id;
-// static TimerHandle_t board_rx_timer_id;
-// static TimerHandle_t board_tx_timer_id;
+static TimerHandle_t board_heartbeat_timer_id;
+static TimerHandle_t board_rx_timer_id;
+static TimerHandle_t board_tx_timer_id;
 
-// static StaticTimer_t board_heartbeat_buffer;
-// static StaticTimer_t board_rx_buffer;
-// static StaticTimer_t board_tx_buffer;
+static StaticTimer_t board_heartbeat_buffer;
+static StaticTimer_t board_rx_buffer;
+static StaticTimer_t board_tx_buffer;
+
+#include "board.h"
+#include "string.h"
+
+u_int32_t SystemCoreClock = 12000000;
 
 bool core_isr_active(void)
 {
     return SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk;
 }
-#include "board.h"
-#include "string.h"
 
-static void board_setup_clocking(void)
+static void board_setup_main_clock(void)
 {
     // power up main oscillator
     LPC_SYSCON->PDRUNCFG &= ~PDRUNCFG_SYSOSC_PD;
@@ -65,7 +69,11 @@ static void board_setup_clocking(void)
     LPC_SYSCON->MAINCLKSEL = 0x03U;
     LPC_SYSCON->MAINCLKUEN = 0x00U;
     LPC_SYSCON->MAINCLKUEN = 0x01U;
+    SystemCoreClock = 48000000;
+}
 
+static void board_setup_uart_clock(void)
+{
     // enable IOCON block
     LPC_SYSCON->SYSAHBCLKCTRL |= SYSAHBCLKCTRL_IOCON;
 
@@ -76,31 +84,28 @@ static void board_setup_clocking(void)
     // set uart clock to 12 MHz
     LPC_SYSCON->UARTCLKDIV = 0x04U;
     LPC_SYSCON->SYSAHBCLKCTRL |= SYSAHBCLKCTRL_UART;
+}
 
-    // enable CLKOUT
-    // LPC_SYSCON->CLKOUTCLKSEL = 0x03; // main clock
-    // LPC_SYSCON->CLKOUTDIV = 0x01;    // divide by 1
-    // LPC_SYSCON->CLKOUTUEN = 0x00;    // update enable - sequence
-    // LPC_SYSCON->CLKOUTUEN = 0x01;
+static void board_setup_clocking(void)
+{
+    board_setup_main_clock();
+    board_setup_uart_clock();
 }
 
 static void board_setup_pin_mux(void)
-{
-    // enable clkout
-    LPC_IOCON->PIO0_1 |= 0x01;
-}
+{}
 
 void board_set_led(enum board_led id)
 {
     if (id == LED_DALI) {
-        LPC_GPIO3->DATA |= (1 << 5);
+        LPC_GPIO3->DATA &= ~(1 << 5);
     }
 }
 
 void board_reset_led(enum board_led id)
 {
     if (id == LED_DALI) {
-        LPC_GPIO3->DATA &= (1 << 5);
+        LPC_GPIO3->DATA |= (1 << 5);
     }
 }
 
@@ -112,25 +117,25 @@ bool board_get_dali_rx(void)
 void board_set_dali_tx(bool state)
 {}
 
-// static void board_heartbeat(TimerHandle_t NOUSE(dummy))
-// {
-//     static uint32_t counter = 0;
+static void board_heartbeat(TimerHandle_t NOUSE(dummy))
+{
+    static uint32_t counter = 0;
 
-//     counter++;
-//     if (board_get_dali_rx()) {
-//         if (counter & 1) {
-//             board_set_led(LED_DALI);
-//         } else {
-//             board_reset_led(LED_DALI);
-//         }
-//     } else {
-//         if (counter > SLOW_HEARTBEAT_COUNTER) {
-//             board_set_led(LED_DALI);
-//             counter = 0;
-//         } else
-//             board_reset_led(LED_DALI);
-//     }
-// }
+    counter++;
+    if (board_get_dali_rx()) {
+        if (counter & 1) {
+            board_set_led(LED_DALI);
+        } else {
+            board_reset_led(LED_DALI);
+        }
+    } else {
+        if (counter > SLOW_HEARTBEAT_COUNTER) {
+            board_set_led(LED_DALI);
+            counter = 0;
+        } else
+            board_reset_led(LED_DALI);
+    }
+}
 
 // static void board_tx_timeout(TimerHandle_t NOUSE(dummy))
 // {
@@ -173,15 +178,16 @@ void board_init(void)
 {
     LOG_THIS_INVOCATION(LOG_FORCE);
 
-    LPC_GPIO3->DIR = (1 << 5);
+    LOG_PRINTF(LOG_FORCE, "GPIO3 direction register at: %08lx", &(LPC_GPIO3->DIR));
+    LPC_GPIO3->DIR |= (1 << 5);
 
-    // LOG_TEST(board_heartbeat_timer_id = xTimerCreateStatic(
-    //              "heart_timer", HEARTBEAT_PERIOD_MS, pdTRUE, NULL, board_heartbeat, &board_heartbeat_buffer));
+    LOG_TEST(board_heartbeat_timer_id = xTimerCreateStatic(
+                 "heart_timer", HEARTBEAT_PERIOD_MS, pdTRUE, NULL, board_heartbeat, &board_heartbeat_buffer));
     // LOG_TEST(board_rx_timer_id =
     //              xTimerCreateStatic("rx_timer", FLASH_PERIOD_MS, pdFALSE, NULL, board_rx_timeout, &board_rx_buffer));
     // LOG_TEST(board_tx_timer_id =
     //              xTimerCreateStatic("tx_timer", FLASH_PERIOD_MS, pdFALSE, NULL, board_tx_timeout, &board_tx_buffer));
-    // xTimerStart(board_heartbeat_timer_id, 0x0);
+    LOG_TEST(xTimerStart(board_heartbeat_timer_id, 0x0));
 }
 
 void board_system_init(void)
