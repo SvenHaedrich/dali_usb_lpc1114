1e571318297410e7d7eb3bfb324c05ecdc341e4c
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-04-22 17:13:17 +0200

    re-format add extra debugging

--- tests/scripts/conftest.py
+++ tests/scripts/conftest.py
@@ -4,8 +4,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 @pytest.fixture(scope="session")
 def serial_conn(port="/dev/ttyUSB0", baudrate=115200):
-    logger.debug(F"open serial port {port}")
-    serial_port = serial.Serial(port=port,baudrate=baudrate, timeout=0.5)
+    logger.debug(f"open serial port {port}")
+    serial_port = serial.Serial(port=port, baudrate=baudrate, timeout=2)
     return serial_port
--- tests/scripts/test_tx_multiple.py
+++ tests/scripts/test_tx_multiple.py
@@ -4,10 +4,12 @@ import time
 
 logger = logging.getLogger(__name__)
 
+
 def read_one_frame(serial_conn, expected_data):
     reply = serial_conn.readline()
-    start = reply.find(ord('{'))+1
-    end = reply.find(ord('}'))
+    logger.debug(f"read line: {reply}")
+    start = reply.find(ord("{")) + 1
+    end = reply.find(ord("}"))
     assert start > 0
     assert end > 0
     payload = reply[start:end]
@@ -20,20 +22,24 @@ def read_one_frame(serial_conn, expected_data):
     assert data == expected_data
     return timestamp
 
-@pytest.mark.parametrize("cmd, settling, data", [
-    ("T0 10 FF00\r", 5500, 0xFF00), 
-    ("T1 10 00FF\r", 13500, 0x00FF),
-    ("T2 10 FF00\r", 14900, 0xFF00),
-    ("T3 10 00FF\r", 16300, 0x00FF),
-    ("T4 10 FF00\r", 17900, 0xFF00),
-    ("T5 10 00FF\r", 19500, 0x00FF),
-    ("R0 1 10 FF00\r", 5500, 0xFF00), 
-    ("R1 1 10 00FF\r", 13500, 0x00FF),
-    ("R2 1 10 FF00\r", 14900, 0xFF00),
-    ("R3 1 10 00FF\r", 16300, 0x00FF),
-    ("R4 1 10 FF00\r", 17900, 0xFF00),
-    ("R5 1 10 00FF\r", 19500, 0x00FF),
-    ])
+
+@pytest.mark.parametrize(
+    "cmd, settling, data",
+    [
+        ("T0 10 FF00\r", 5500, 0xFF00),
+        ("T1 10 00FF\r", 13500, 0x00FF),
+        ("T2 10 FF00\r", 14900, 0xFF00),
+        ("T3 10 00FF\r", 16300, 0x00FF),
+        ("T4 10 FF00\r", 17900, 0xFF00),
+        ("T5 10 00FF\r", 19500, 0x00FF),
+        ("R0 1 10 FF00\r", 5500, 0xFF00),
+        ("R1 1 10 00FF\r", 13500, 0x00FF),
+        ("R2 1 10 FF00\r", 14900, 0xFF00),
+        ("R3 1 10 00FF\r", 16300, 0x00FF),
+        ("R4 1 10 FF00\r", 17900, 0xFF00),
+        ("R5 1 10 00FF\r", 19500, 0x00FF),
+    ],
+)
 def test_settling_priority(serial_conn, cmd, settling, data):
     serial_conn.write(cmd.encode("utf-8"))
     timestamp_1 = read_one_frame(serial_conn, data)
@@ -42,24 +48,28 @@ def test_settling_priority(serial_conn, cmd, settling, data):
     fullbit_time = 833 / 1000000
     expected_delta = 17 * fullbit_time + (settling / 1000000)
     tolerance = 1 / 1000
-    logger.debug(F"delta is {delta} expected is {expected_delta}")
+    logger.debug(f"delta is {delta} expected is {expected_delta}")
     assert (abs(delta - expected_delta)) < tolerance
 
-@pytest.mark.parametrize("repeat, data", [
-    (0, 0x1000),
-    (1, 0x1111),
-    (2, 0x2020),
-    (3, 0x3300),
-    (4, 0x4004),
-    (8, 0x8000),
-    (16, 0xF000),
-    (32, 0x1234),
-    (64, 0x5FF5),
-    (127, 0x1212),
-    (255, 0xFFFF)
-])
-def test_repeat(serial_conn,repeat,data):
-    cmd = F"R0 {repeat:x} 10 {data:x}\r"
+
+@pytest.mark.parametrize(
+    "repeat, data",
+    [
+        (0, 0x1000),
+        (1, 0x1111),
+        (2, 0x2020),
+        (3, 0x3300),
+        (4, 0x4004),
+        (8, 0x8000),
+        (16, 0xF000),
+        (32, 0x1234),
+        (64, 0x5FF5),
+        (127, 0x1212),
+        (255, 0xFFFF),
+    ],
+)
+def test_repeat(serial_conn, repeat, data):
+    cmd = f"R0 {repeat:x} 10 {data:x}\r"
     serial_conn.write(cmd.encode("utf-8"))
-    for j in range(repeat+1):
+    for j in range(repeat + 1):
         read_one_frame(serial_conn, data)
