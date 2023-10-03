bcd27882747ae25fe38c09641c6e219c43896284
SvenHaedrich <sven.haedrich@sevenlab.de>
2023-10-03 13:21:48 +0200

    Add a test for problem

---
+++ tests/scripts/test_40_query.py
@@ -0,0 +1,23 @@
+import pytest
+import logging
+
+from connection.frame import DaliFrame, DaliStatus
+from connection.serial import DaliSerial
+
+logger = logging.getLogger(__name__)
+timeout_time_sec = 2
+
+
+def test_queries(dali_serial) -> None:
+    dali_serial.start_receive()
+    test_cmd_1 = 0xFF01
+    test_frame_1 = DaliFrame(length=16, data=test_cmd_1)
+    dali_serial.query_reply(test_frame_1)
+    assert dali_serial.rx_frame.status.status == DaliStatus.TIMEOUT
+    test_cmd_2 = 0xFF02
+    test_frame_2 = DaliFrame(length=16, data=test_cmd_2)
+    dali_serial.transmit(test_frame_2)
+    dali_serial.get_next()
+    assert dali_serial.rx_frame.status.status == DaliStatus.LOOPBACK
+    assert dali_serial.rx_frame.length == 16
+    assert dali_serial.rx_frame.data == test_cmd_2
