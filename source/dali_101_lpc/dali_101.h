2aa6c5a4adce1933c3a88feb547499c71cc18bcc
SvenHaedrich <sven.haedrich@sevenlab.de>
2024-01-07 17:25:34 +0100

    Reword doxygen documentation

--- source/dali_101_lpc/dali_101.h
+++ source/dali_101_lpc/dali_101.h
@@ -34,90 +34,90 @@ enum dali_status {
 
 /**
  * @brief DALI received frame
- * 
+ *
  */
 struct dali_rx_frame {
-    bool loopback;              /**< frame was received while transmission was active*/
-    enum dali_status status;    /**< status at the end of frame receiption */
-    uint8_t length;             /**< number of data bits received */
-    uint32_t data;              /**< data payload */
-    uint32_t timestamp;         /**< timetstamp when start bit was deteceted */
+    bool loopback;           /**< frame was received while transmission was active*/
+    enum dali_status status; /**< status at the end of frame receiption */
+    uint8_t length;          /**< number of data bits received */
+    uint32_t data;           /**< data payload */
+    uint32_t timestamp;      /**< timetstamp when start bit was deteceted */
 };
 
 /**
  * @brief DALI transmission frame
- * 
+ *
  */
 struct dali_tx_frame {
-    bool is_query;                      /**< check for a backward frame */
-    bool is_corrupt;                    /**< tranmsit a corrupt frame (disregard `length` and `data`)*/
-    uint8_t length;                     /**< number of data bits to transmit */
-    uint32_t data;                      /**< data payload */
-    uint8_t repeat;                     /**< repeat entire frame */
-    enum dali_tx_priority priority;     /**< inter frame timing priority */
+    bool is_query;                  /**< check for a backward frame */
+    bool is_corrupt;                /**< tranmsit a corrupt frame (disregard `length` and `data`)*/
+    uint8_t length;                 /**< number of data bits to transmit */
+    uint32_t data;                  /**< data payload */
+    uint8_t repeat;                 /**< repeat entire frame */
+    enum dali_tx_priority priority; /**< inter frame timing priority */
 };
 
 /**
  * @brief Initialize the DALI low level driver
- * 
+ *
  */
 void dali_101_init(void);
 
 /**
  * @brief Send DALI frame
- * 
+ *
  * @param frame frame to send
  */
 void dali_101_send(const struct dali_tx_frame frame);
 
 /**
  * @brief Get the next frame from the input queue
- * 
+ *
  * @param frame received frame
  * @param wait number of ticks to wait until a frame is available in queue
- * @return true a frame was received and is availabe
- * @return false no frame availabe, discard buffer
+ * @return `true` - a frame was received and is availabe
+ * @return `false`-  no frame availabe, discard buffer
  */
 bool dali_101_get(struct dali_rx_frame* frame, TickType_t wait);
 
 /**
  * @brief Check if a transmission is active or pending
- * 
- * @return true no transmission 
- * @return false tranmssion is active
+ *
+ * @return `true` - no transmission
+ * @return `false` - transmission is active
  */
 bool dali_101_tx_is_idle(void);
 
 /**
  * @brief Start a new bit sequence, discard old sequence information
- * 
+ *
  */
 void dali_101_sequence_start(void);
 
 /**
  * @brief Define next period for sequence
- * 
+ *
  * @param period_us duration for the next period, given in micro seconds
  */
 void dali_101_sequence_next(uint32_t period_us);
 
 /**
- * @brief Send the sequence 
- * 
+ * @brief Send the sequence
+ *
  */
 void dali_101_sequence_execute(void);
 
 /**
  * @brief request a status frame
- * 
+ *
  * Request a status frame, even when there is no bus activity.
  * This is a soon to be obsolete function.
  */
 void dali_101_request_status_frame(void);
 
-/* callback functions defined by the low level driver 
-*  to be called by the board interface module
-*/
+/* callback functions defined by the low level driver
+ *  to be called by the board interface module
+ */
 void dali_tx_irq_callback(void);
 void dali_rx_irq_capture_callback(void);
 void dali_rx_irq_stopbit_match_callback(void);
